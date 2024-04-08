534e5564e9bdcd94e23a2498b8fed2724e2adabd
absolutelySalvo <Salvatore.Romeo.12@outlook.it>
2024-04-08 21:04:24 +0200

    FIX REDESIGN

--- javascript/script.js
+++ javascript/script.js
@@ -5,22 +5,23 @@ let navLinks = document.querySelectorAll('header nav a');
 
 
 window.onscroll = () => {
-        section.forEach(sec => {
-            let top = window.scrollY;
-            let offset = sec.offsetTop - 150;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id');
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach((links) => {
-                    links.classList.remove('active');
-                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-                });
-            }
-        }); 
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 }
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
+});
